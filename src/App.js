2f99e0c2eea9cc950bef71541b541b3c9158f585
Karen Marlen <72042911+karen-marlen@users.noreply.github.com>
2021-01-29 12:58:05 -0300

    app edit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 import './App.css';
 
+const projectID = 'ffca04a4-6104-4245-bc0f-ea552682e7d1';
+
 const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />;
+
     return (
         <ChatEngine 
             height="100vh"
-            projectID="ffca04a4-6104-4245-bc0f-ea552682e7d1"
-            userName="karen-marlen"
-            userSecret="12345678"
+            projectID={ projectID }
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={( chatAppProps ) => <ChatFeed { ...chatAppProps } /> }
+            onNewMessage={() => Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
         />
     );
-}
+};
 
 export default App;
