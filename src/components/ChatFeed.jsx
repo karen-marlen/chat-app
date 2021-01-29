f5378e8938e24ff125e081e7423839b6dfc8f557
Karen Marlen <72042911+karen-marlen@users.noreply.github.com>
2021-01-29 11:44:47 -0300

    render message

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -18,8 +18,19 @@ const ChatFeed = (props) => {
             const isMyMessage = userName === message.sender.username;
 
             return (
-                <div>
+                <div key={`msg_${index}`} style={{ width: '100%' }}>
 
+                    <div className='message-block'>
+                        {
+                            isMyMessage
+                            ? <MyMessage />
+                            : <TheirMessage />
+                        }
+                    </div>
+
+                    <div className='read-receipts' style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
+                        read-receipts
+                    </div>
                 </div>
             );
         })
