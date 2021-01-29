6fa24dd56708194cea76ccc503299de8e5b05b6d
Karen Marlen <72042911+karen-marlen@users.noreply.github.com>
2021-01-29 11:01:33 -0300

    app :computer: chat-engine

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="ffca04a4-6104-4245-bc0f-ea552682e7d1"
+            userName="karen-marlen"
+            userSecret="12345678"
+        />
+    );
+}
+
+export default App;
